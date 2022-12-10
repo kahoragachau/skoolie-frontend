bdf5d1fa4e0ffd14f5bceb9b096a85bce1f7a167
kahoragachau <joelkahoragachau6@gmail.com>
2022-12-10 22:56:18 +0300

    - Wrap indexjs with Chakra provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import Signup from "./Signup";
-import { ChakraProvider } from "@chakra-ui/react";
+// import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <ChakraProvider>
-    {/* <h1> Hello, World!!</h1>
-    <h2> Signup Form</h2> */}
-    <Signup />
-   </ChakraProvider>
+    <>
+      <Signup />
+    </>
   );
 }
 
