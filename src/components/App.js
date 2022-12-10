51074d39ca137f09e19025d297a3560cfd095b4c
kahoragachau <joelkahoragachau6@gmail.com>
2022-12-10 23:20:44 +0300

    - Add login route

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,9 @@
 import Signup from "./Signup";
-// import { ChakraProvider } from "@chakra-ui/react";
 import Login from "./Login";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    // <>
-    //   <Signup />
-    // </>
     <Routes>
       <Route path="/" exact element={<Signup />} />
       <Route path="/login" exact element={<Login />} />
