c14afbc91b2543e8ad2dbeb56b651ed17679cc6e
kahoragachau <joelkahoragachau6@gmail.com>
2022-12-10 15:45:13 +0300

    - Install tailwindcss dependencies

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
+import Signup from "./Signup";
+
+
 function App() {
   return (
-   <h1> Hello, World!!</h1>
+    <>
+    <h1> Hello, World!!</h1>
+    <h2> Signup Form</h2>
+    <Signup />
+   </>
   );
 }
 
---
+++ src/components/Signup.js
@@ -0,0 +1,10 @@
+function Signup(){
+   return(
+    <>
+        <label>Name</label>
+        <input type="text"></input>
+    </>
+   )
+}
+
+export default Signup;
